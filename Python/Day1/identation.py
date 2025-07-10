b9877446ee9cda05858298ab307aa471ef226eaa
ajayrana06 <ajay07503@gmail.com>
2025-07-10 23:06:54 +0530

    Add files via upload
    Day1

---
+++ Python/Day1/identation.py
@@ -0,0 +1,16 @@
+#with indentation
+
+name='sam'
+if name == 'sam':
+    print("Hello")
+else:
+    print('ajay')
+
+#with indentation
+
+name='rocky'
+if name == 'rocky':
+print("Hello")
+else:
+print('ajay')
+print('ajay')
---
+++ Python/Day1/type_function.py
@@ -0,0 +1,5 @@
+#type functioon is used for knowing the type of variable.
+x=6
+y="Ajay"
+print(type(x))
+print(type(y))
